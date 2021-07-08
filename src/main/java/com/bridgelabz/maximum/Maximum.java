64ecf6185cfd5853be4b898ed2343f70edc7d0cb
Naveen2706 <naveen271996@gmail.com>
2021-07-08 21:39:53 +0530

    Maximum of three floats with TestMaximum

--- src/main/java/com/bridgelabz/maximum/Maximum.java
+++ src/main/java/com/bridgelabz/maximum/Maximum.java
@@ -4,21 +4,28 @@ public class Maximum {
 
 	public static void main(String[] args) {
 		Maximum maximum = new Maximum();
-		Integer firstValue = 10;
-		Integer secondValue = 15;
-		Integer thirdValue = 20;
-		
+		Float firstValue = 18.6f, secondValue = 16.3f, thirdValue = 12.5f;
 		System.out.println(maximum.getMax(firstValue, secondValue, thirdValue));
-		
-		
-	}
-    public Integer getMax(Integer firstValue,Integer secondValue,Integer thirdValue){
-        Integer max = firstValue;
-        if (secondValue.compareTo(max) > 0)
-            max = secondValue;
-        if (thirdValue.compareTo(max) > 0)
-            max = thirdValue;
 
-        return max;
-    }
+	}
+
+	public Integer getMax(Integer firstValue, Integer secondValue, Integer thirdValue) {
+		Integer max = firstValue;
+		if (secondValue.compareTo(max) > 0)
+			max = secondValue;
+		if (thirdValue.compareTo(max) > 0)
+			max = thirdValue;
+
+		return max;
+	}
+
+	public Float getMax(Float firstValue, Float secondValue, Float thirdValue) {
+		Float max = firstValue;
+		if (secondValue.compareTo(max) > 0)
+			max = secondValue;
+		if (thirdValue.compareTo(max) > 0)
+			max = thirdValue;
+
+		return max;
+	}
 }
--- src/test/java/com/bridgelabz/maximum/TestMaximum.java
+++ src/test/java/com/bridgelabz/maximum/TestMaximum.java
@@ -4,23 +4,41 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMaximum {
-    Maximum maximum = new Maximum();
+	Maximum maximum = new Maximum();
 
-    @Test
-    public void givenIntegerFirstNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Integer expected = 90 ;
-        Assert.assertEquals(expected,maximum.getMax(90,40,10));
-    }
+	@Test
+	public void givenIntegerFirstNumberGreater_WhenLargest_ShouldReturnTrue() {
+		Integer expected = 90;
+		Assert.assertEquals(expected, maximum.getMax(90, 40, 10));
+	}
 
-    @Test
-    public void givenIntegerSecondNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Integer expected = 90 ;
-        Assert.assertEquals(expected,maximum.getMax(40,90,10));
-    }
+	@Test
+	public void givenIntegerSecondNumberGreater_WhenLargest_ShouldReturnTrue() {
+		Integer expected = 90;
+		Assert.assertEquals(expected, maximum.getMax(40, 90, 10));
+	}
 
-    @Test
-    public void givenIntegerThirdNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Integer expected = 90 ;
-        Assert.assertEquals(expected,maximum.getMax(40,10,90));
-    }
+	@Test
+	public void givenIntegerThirdNumberGreater_WhenLargest_ShouldReturnTrue() {
+		Integer expected = 90;
+		Assert.assertEquals(expected, maximum.getMax(40, 10, 90));
+	}
+
+	@Test
+	public void givenFloatFirstNumberGreater_WhenLargest_ShouldReturnTrue() {
+		Float expected = 5.6f;
+		Assert.assertEquals(expected, maximum.getMax(5.6f, 4.2f, 2.2f));
+	}
+
+	@Test
+	public void givenFloatSecondNumberGreater_WhenLargest_ShouldReturnTrue() {
+		Float expected = 5.6f;
+		Assert.assertEquals(expected, maximum.getMax(2.2f, 5.6f, 4.3f));
+	}
+
+	@Test
+	public void givenFloatThirdNumberGreater_WhenLargest_ShouldReturnTrue() {
+		Float expected = 5.6f;
+		Assert.assertEquals(expected, maximum.getMax(1.2f, 4.2f, 5.6f));
+	}
 }
